7a30fb5f71d958e9db3d51f4eb639ab7374680f5
Tyler Hatch <tylerhatch955@gmail.com>
2018-10-08 19:36:51 -0500

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :pokemons, only: [:index, :show]
   resources :moves, only: [:index, :show]
   resources :types, only: [:index, :show]
+
+  root to: 'pokemons#index'
 end
