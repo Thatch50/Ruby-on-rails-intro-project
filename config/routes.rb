06082599ec74a7d2ce961008953f06b31ea5aef3
Tyler Hatch <tylerhatch955@gmail.com>
2018-10-08 19:36:25 -0500

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'types/show'
-  get 'types/index'
-  get 'moves/show'
-  get 'moves/index'
-  get 'pokemons/show'
-  get 'pokemons/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pokemons, only: [:index, :show]
+  resources :moves, only: [:index, :show]
+  resources :types, only: [:index, :show]
 end
